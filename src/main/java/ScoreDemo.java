70c8bda4eb70349b06d2cc099f528c7c2fd9e0d3
Fernando <faavanso@gmail.com>
2021-10-23 23:18:50 -0400

    Adjusts

--- src/main/java/ScoreDemo.java
+++ src/main/java/ScoreDemo.java
@@ -10,38 +10,25 @@ import java.util.Scanner;
 public class ScoreDemo {
 
     public static void main(String[] args) throws FileNotFoundException {
+
+
         ScoreList list = new ScoreList();
-        //ScoreNode head = new ScoreNode(null, 0);
-        list.setHead(new ScoreNode("fe", 2));
-        ScoreNode second = new ScoreNode("Carlos", 5);
-        ScoreNode third = new ScoreNode("Jose", 10);
-
-        list.getHead().setNextNode(second);
-        second.setNextNode(third);
-        //System.out.println(list.getHead());
-        list.print();
+        ScoreList temp = new ScoreList();
+        Scanner keyboard = new Scanner(System.in);
 
 
+        // Open the file.
+        File file = new File("scores.txt");
+        Scanner inputFile = new Scanner(file);
+        while (inputFile.hasNext()) {
+            temp.add(inputFile.next(), inputFile.nextInt());
+        }
 
+        inputFile.close();
+        temp.print();
 
-//        ScoreList list = new ScoreList();
-//        ScoreList temp = new ScoreList();
-//        Scanner keyboard = new Scanner(System.in);
-//
-//
-//        // Open the file.
-//        File file = new File("scores.txt");
-//        Scanner inputFile = new Scanner(file);
-//        while (inputFile.hasNext()) {
-//            temp.addToList(inputFile.next(), inputFile.nextInt());
-//        }
-//
-//        inputFile.close();
-//        temp.print();
-//        System.out.println(temp);
-//
-//        int decision = 0;
-//
+        int decision = 0;
+
 //        System.out.println("Would you like to add another (1), remove (2), or quit the program (3)?");
 //        decision = keyboard.nextInt();
 //        while (decision != 3) {
@@ -67,6 +54,6 @@ public class ScoreDemo {
 //
 //
 //        }
-//
+
     }
 }
--- src/main/java/ScoreList.java
+++ src/main/java/ScoreList.java
@@ -14,50 +14,52 @@ public class ScoreList {
     public void setHead(ScoreNode head) {
         this.head = head;
     }
-    public ScoreNode getHead(){
+
+    public ScoreNode getHead() {
         return head;
     }
 
-    ScoreList(ScoreList scoreCopy) {
-        for(ScoreNode pointer = scoreCopy.head; pointer != null; pointer = pointer.getNextNode()){
-            addToList(pointer.getName(),pointer.getScore());
-        }
-    }
+//    ScoreList(ScoreList scoreCopy) {
+//        for(ScoreNode pointer = scoreCopy.head; pointer != null; pointer = pointer.getNextNode()){
+//            addToList(pointer.getName(),pointer.getScore());
+//        }
+//    }
 
-
-    public void addToList( String name, int score) {
+    public void add(String name, int score) {
         ScoreNode newNode = new ScoreNode(name, score);
-        if (head == null){
-            head = newNode;
-        }else {
-            ScoreNode pointer = head;
-            while (pointer.getNextNode()!= null && pointer.getNextNode().getScore() > newNode.getScore()){
+        if (this.head == null) {
+            //newNode.setNextNode(head);
+            this.setHead(newNode);
+        } else {
+            ScoreNode pointer = new ScoreNode(name, score);
+            this.setHead(pointer);
+            while (pointer.getNextNode() != null && pointer.getNextNode().getScore() > newNode.getScore()) {
                 pointer = pointer.getNextNode();
             }
-            if (pointer == head && pointer.getScore() < newNode.getScore()){
-                newNode.getNextNode().setNextNode(head);
-                head = newNode;
-            }else {
-                newNode.setNextNode(pointer);
+            if (pointer == this.getHead() && pointer.getScore() < newNode.getScore()) {
+                newNode.setNextNode(head);
+                this.setHead(newNode);
+            } else {
+                newNode.setNextNode(pointer.getNextNode());
                 pointer.setNextNode(newNode);
             }
         }
+
     }
 
-
-    public void remove (String name){
-        ScoreNode tempNode = head, preview = null;
-        if(tempNode != null && tempNode.getName().equalsIgnoreCase(name)){
+    public void remove(String name) {
+        ScoreNode tempNode = this.head, preview = null;
+        if (tempNode != null && tempNode.getName().equalsIgnoreCase(name)) {
             head = tempNode.getNextNode();
             return;
         }
 
-        while(tempNode != null && !tempNode.getName().equalsIgnoreCase(name)){
+        while (tempNode != null && !tempNode.getName().equalsIgnoreCase(name)) {
             preview = tempNode;
             tempNode = tempNode.getNextNode();
         }
 
-        if (tempNode == null){
+        if (tempNode == null) {
             System.out.println("NAME NOT FOUND");
             return;
         }
@@ -65,10 +67,10 @@ public class ScoreList {
         preview.getNextNode().setNextNode(tempNode);
     }
 
-    public boolean verifyToUpdate(String name){
+    public boolean verifyToUpdate(String name) {
         ScoreNode tempNode = head;
-        while (tempNode.getNextNode()!=null){
-            if(tempNode.getName().equalsIgnoreCase(name)) {
+        while (tempNode.getNextNode() != null) {
+            if (tempNode.getName().equalsIgnoreCase(name)) {
                 return true;
             }
             tempNode = tempNode.getNextNode();
@@ -76,13 +78,13 @@ public class ScoreList {
         return false;
     }
 
-    public void print(){
+    public void print() {
         ScoreNode node = this.getHead();
-        while (node != null)
-        {
-            System.out.print(node.getName()+ " " + node.getScore()+ "\n");
+        while (node != null) {
+            System.out.print(node.getName() + " " + node.getScore() + "\n");
             node = node.getNextNode();
         }
     }
+
 
 }
